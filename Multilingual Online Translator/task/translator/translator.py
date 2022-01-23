30eabffba402e5e43fb9848e578ce107d936ce0b
rolmos14 <olmos.rafa@gmail.com>
2022-01-23 21:37:40 +0100

    Stage 1/7. Initial version

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,20 @@
-print('Hello, World!')
+class Translator:
+
+    languages = {"en": "English",
+                 "fr": "French"}
+
+    def __init__(self):
+        self.target_language = ""
+        self.text = ""
+
+    def translate(self):
+        print(f'Type "en" if you want to translate from {self.languages["fr"]} into {self.languages["en"]}, '
+              f'or "fr" if you want to translate from {self.languages["en"]} into {self.languages["fr"]}:')
+        self.target_language = input()
+        print("Type the word you want to translate:")
+        self.text = input()
+        print(f'You chose "{self.target_language}" as the language to translate "{self.text}" to.')
+
+
+translator = Translator()
+translator.translate()
